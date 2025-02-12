be4f4e0f2598a604df4ce48abef3f31f284024cd
Artificer-Cat <chemestru@gmail.com>
2025-02-12 13:37:06 +0300

    Переписывание кода сериализации и десериализации транзакций. Добавлена возможность сериализовать в слайс транзакций в байтовый слайс и восстанавливать транзакции из байтового слайса

--- server/internal/services/transaction/transaction.go
+++ server/internal/services/transaction/transaction.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"encoding/binary"
 
 	"golang_blockchain/pkg/block"
 	"golang_blockchain/pkg/iterator"
@@ -243,4 +244,229 @@ func (b *Transaction) BytesToTransaction(clice []byte) error {
 /* Перевод выхода в строку */
 func TransactionOutputToString(to TransactionOutput) string {
 	return string(to.Hash)
+}
+
+
+/*
+=======================================================
+========= Функции сериализации/десериализации =========
+=======================================================
+*/
+
+/*
+SerializeTransactions сериализует слайс транзакций в байтовый слайс
+
+Аргументы:
+  - []Transaction: transactions список транзакций для записи
+
+Возвращает:
+  - error: ошибка
+  - []byte: готовый байтовый слайс с транзакциями
+*/
+func SerializeTransactions(transactions []Transaction) ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// Запись количества транзакций
+	if err := binary.Write(buf, binary.LittleEndian, uint32(len(transactions))); err != nil {
+		return nil, err
+	}
+	// Запись каждой транзакции 
+	for _, tx := range transactions {
+		// Запись ID
+		if err := writeBytes(buf, tx.ID); err != nil {
+			return nil, err
+		}
+		// Запись времени создания
+		if err := binary.Write(buf, binary.LittleEndian, tx.TimeOfCreation); err != nil {
+			return nil, err
+		}
+
+		// Запись количества входов
+		if err := binary.Write(buf, binary.LittleEndian, uint32(len(tx.Inputs))); err != nil {
+			return nil, err
+		}
+		// Запись самих входов
+		for _, in := range tx.Inputs {
+			if err := writeBytes(buf, in.PreviousTransactionID); err != nil {
+				return nil, err
+			}
+			if err := writeBytes(buf, in.PreviousOutputHash); err != nil {
+				return nil, err
+			}
+			if err := writeBytes(buf, in.PublicKey); err != nil {
+				return nil, err
+			}
+		}
+
+		// Запись количества выходов
+		if err := binary.Write(buf, binary.LittleEndian, uint32(len(tx.Outputs))); err != nil {
+			return nil, err
+		}
+		// Запись самих выходов
+		for _, out := range tx.Outputs {
+			// Записываем Value как int32.
+			if err := binary.Write(buf, binary.LittleEndian, int32(out.Value)); err != nil {
+				return nil, err
+			}
+			if err := writeBytes(buf, out.RecipientAddress); err != nil {
+				return nil, err
+			}
+			if err := binary.Write(buf, binary.LittleEndian, out.TimeOfCreation); err != nil {
+				return nil, err
+			}
+			if err := writeBytes(buf, out.Hash); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return buf.Bytes(), nil
+}
+
+/*
+SerializeTransactions восстанавливает слайс транзакций из байтового слайса
+
+Аргументы:
+  - []byte: data слайс с закодированными транзакциями
+
+Возвращает:
+  - error: ошибка
+  - []Transaction: список транзакций
+*/
+func DeserializeTransactions(data []byte) ([]Transaction, error) {
+	buf := bytes.NewReader(data)
+	var txCount uint32
+	if err := binary.Read(buf, binary.LittleEndian, &txCount); err != nil {
+		return nil, err
+	}
+
+	txs := make([]Transaction, txCount)
+	for i := uint32(0); i < txCount; i++ {
+		var tx Transaction
+
+		id, err := readBytes(buf)
+		if err != nil {
+			return nil, err
+		}
+		tx.ID = id
+
+		if err := binary.Read(buf, binary.LittleEndian, &tx.TimeOfCreation); err != nil {
+			return nil, err
+		}
+
+		// Чтение количества входов
+		var inCount uint32
+		if err := binary.Read(buf, binary.LittleEndian, &inCount); err != nil {
+			return nil, err
+		}
+		// Чтение самих входов 
+		tx.Inputs = make([]TransactionInput, inCount)
+		for j := uint32(0); j < inCount; j++ {
+			var in TransactionInput
+
+			prevTxID, err := readBytes(buf)
+			if err != nil {
+				return nil, err
+			}
+			in.PreviousTransactionID = prevTxID
+
+			prevOutHash, err := readBytes(buf)
+			if err != nil {
+				return nil, err
+			}
+			in.PreviousOutputHash = prevOutHash
+
+			pubKey, err := readBytes(buf)
+			if err != nil {
+				return nil, err
+			}
+			in.PublicKey = pubKey
+
+			tx.Inputs[j] = in
+		}
+
+		// Чтение количества выходов
+		var outCount uint32
+		if err := binary.Read(buf, binary.LittleEndian, &outCount); err != nil {
+			return nil, err
+		}
+		// Чтение самих выходов
+		tx.Outputs = make([]TransactionOutput, outCount)
+		for k := uint32(0); k < outCount; k++ {
+			var out TransactionOutput
+
+			var value int32
+			if err := binary.Read(buf, binary.LittleEndian, &value); err != nil {
+				return nil, err
+			}
+			out.Value = int(value)
+
+			recip, err := readBytes(buf)
+			if err != nil {
+				return nil, err
+			}
+			out.RecipientAddress = recip
+
+			if err := binary.Read(buf, binary.LittleEndian, &out.TimeOfCreation); err != nil {
+				return nil, err
+			}
+
+			hash, err := readBytes(buf)
+			if err != nil {
+				return nil, err
+			}
+			out.Hash = hash
+
+			tx.Outputs[k] = out
+		}
+		txs[i] = tx
+	}
+	return txs, nil
+}
+
+/*
+writeBytes записывает слайс байтов в буфер, 
+предварительно записывая его длину (uint32)
+
+Аргументы:
+  - *bytes.Buffer: buf указатель на буфер для записи
+  - []byte: data данные для записи в буфер
+
+Возвращает:
+  - error: ошибка
+*/
+func writeBytes(buf *bytes.Buffer, data []byte) error {
+	if err := binary.Write(buf, binary.LittleEndian, uint32(len(data))); err != nil {
+		return err
+	}
+	if _, err := buf.Write(data); err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+SerializeTransactions считывает длинну слайса из буфера,
+и восстанавливает закодтрованный в буфере слайс
+
+Аргументы:
+  - *bytes.Buffer: buf указатель на буфер для чтения
+
+Возвращает:
+  - error: ошибка
+  - []byte: список транзакций
+*/
+func readBytes(buf *bytes.Reader) ([]byte, error) {
+	var length uint32
+	if err := binary.Read(buf, binary.LittleEndian, &length); err != nil {
+		return nil, err
+	}
+	data := make([]byte, length)
+	n, err := buf.Read(data)
+	if err != nil {
+		return nil, err
+	}
+	if uint32(n) != length {
+		return nil, fmt.Errorf("expected %d bytes, got %d", length, n)
+	}
+	return data, nil
 }
